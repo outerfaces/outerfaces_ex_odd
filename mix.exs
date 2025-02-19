1c4974bb50d4eaa7e8415d3bc62cd03e86fa93c4
rgtj2 <rj.tripp@alignedto.dev>
2025-02-18 20:27:09 -0600

    Update outerfaces to 0.2.2

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule OuterfacesOdd.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:outerfaces, "~> 0.2.1"},
+      {:outerfaces, "~> 0.2.2"},
       {:jason, "~> 1.4"}
     ]
   end
