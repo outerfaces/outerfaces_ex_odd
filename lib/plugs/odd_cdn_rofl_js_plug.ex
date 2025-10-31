45077d581a08874d72a1f7354e2d6f54897ec6c4
RJ Tripp <rj.tripp@alignedto.dev>
2025-10-31 00:24:34 -0500

    Update rofl js plug to handle other flavors of export

--- lib/plugs/odd_cdn_rofl_js_plug.ex
+++ lib/plugs/odd_cdn_rofl_js_plug.ex
@@ -1,7 +1,7 @@
 defmodule Outerfaces.Odd.Plugs.OddCDNRoflJSPlug do
   # These magical regexes are used to match and replace import / export statements in JavaScript files
   @local_cdn_imports_regex ~r/import\s+\{\s*([\s\S]*?)\s*\}\s+from\s+['"][^'"]*\[OUTERFACES_LOCAL_CDN\]\/([^'"]*)['"]\s*;?/is
-  @local_cdn_exports_regex ~r/export\s+\{\s*([\s\S]*?)\s*\}\s+from\s+['"][^'"]*\[OUTERFACES_LOCAL_CDN\]\/([^'"]*)['"]\s*;?/is
+  @local_cdn_exports_regex ~r/export\s+(?:\{\s*([\s\S]*?)\s*\}|\*(?:\s+as\s+(\w+))?)\s+from\s+['"][^'"]*\[OUTERFACES_LOCAL_CDN\]\/([^'"]*)['"]\s*;?/is
 
   @spec transform_javascript_cdn_imports_and_exports(
           file_content :: String.t(),
@@ -58,7 +58,7 @@ defmodule Outerfaces.Odd.Plugs.OddCDNRoflJSPlug do
     )
   end
 
-  @spec replace_cdn_exports(
+@spec replace_cdn_exports(
           file_body :: String.t(),
           cdn_protocol :: String.t(),
           cdn_host :: String.t(),
@@ -74,12 +74,19 @@ defmodule Outerfaces.Odd.Plugs.OddCDNRoflJSPlug do
              is_binary(cdn_protocol) and
              is_binary(cdn_host) and
              is_integer(cdn_port) do
-    replacement = "export {\\1} from '#{cdn_protocol}://#{cdn_host}:#{cdn_port}/\\2'"
-
     Regex.replace(
       @local_cdn_exports_regex,
       file_body,
-      replacement
+      fn _match, g1, g2, g3 ->
+        # g1 = named exports or empty
+        # g2 = namespace name (for "* as name") or empty
+        # g3 = file path
+        cond do
+          g1 != "" -> "export {#{g1}} from '#{cdn_protocol}://#{cdn_host}:#{cdn_port}/#{g3}'"
+          g2 != "" -> "export * as #{g2} from '#{cdn_protocol}://#{cdn_host}:#{cdn_port}/#{g3}'"
+          true -> "export * from '#{cdn_protocol}://#{cdn_host}:#{cdn_port}/#{g3}'"
+        end
+      end
     )
   end
 end
